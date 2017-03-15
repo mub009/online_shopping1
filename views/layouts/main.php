adb3c3807ed628eb4914fbd9b0b1b1184d4ed292
Rufaidashabeer <rufaidapoovancheeri@gmail.com>
2017-03-15 16:44:52 +0530

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -57,7 +57,7 @@ AppAsset::register($this);
     NavBar::end();
     ?>
 
-    <div class="container">
+    <div class="container" style="margin-top:100px">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
