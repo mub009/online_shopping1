76aca33347c26bd980152a1beb527a8513cc1eb8
Rufaidashabeer <rufaidapoovancheeri@gmail.com>
2017-03-16 15:53:24 +0530

    Update index.php

--- views/login/index.php
+++ views/login/index.php
@@ -1,4 +1,11 @@
 <?php
+/**
+* @author rufaida p
+* @version 1.0
+* @created date : 15/03/2017
+* @modified date : 16/03/2017
+*/
+
 /* @var $this yii\web\View */
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
@@ -11,10 +18,11 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $form->field($model, 'username') ?>
 
-    <?= $form->field($model, 'password') ?>
+    <?= $form->field($model, 'password')->passwordInput();?>
 
     <div class="form-group">
         <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
     </div>
 
 <?php ActiveForm::end(); ?>
+
