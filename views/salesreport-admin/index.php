b1a55c9ddcc96188cd30b978c696bbb2f963f6e6
ATHIRA KRISHNAN <ATHIRA KRISHNAN>
2017-03-16 13:25:36 -0700

    first commit1

--- views/salesreport-admin/index.php
+++ views/salesreport-admin/index.php
@@ -11,7 +11,6 @@ use yii\widgets\ActiveField;
 use yii\helpers\ArrayHelper;
 
 ?>
-<br><br>
 <h2><b><u><font color="green">SALES REPORT</font></u></h2><br>
 <div style="float: left; height: 100px; width: 200px">
 <?php $form = ActiveForm::begin(); ?>
--- views/salesreport-admin/insert.php
+++ views/salesreport-admin/insert.php
@@ -5,13 +5,20 @@
 * @created date : 13/03/2017
 * @modified date : 14/03/2017
 */
-
-//print_r($data);
-
+?>
+<table>
+<th>Purchase Date</th>
+<th>Product</th>
+<?php
 $count=count($data);
 for ($i=0; $i <$count ; $i++) 
 { 
-	echo $data[$i]['dat_order_date']."<br>";
+	?>
 
+	<tr><td><?=$data[$i]['dat_order_date']."<br>";?></td>
+	<td><?=$data[$i]['Vchr_product_name']."<br>";?></td></tr>
+<?php
 }
+
 ?>
+</table>
