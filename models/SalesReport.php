3e6984930e05ec44dbf844a9f2cbd545cd40799f
Athirakrishnanp <athirakrishnanp94@gmail.com>
2017-03-16 00:59:44 -0700

    Update SalesReport.php

--- models/SalesReport.php
+++ models/SalesReport.php
@@ -34,10 +34,12 @@ class SalesReport extends Model
  /**
  * function comparedate() for comparing date from interface and  from database 
  */
- public function comparedate()
+ public function comparedate($from,$to)
     {
-        return $data = Yii::$app->db->createCommand("select dat_order_date from tbl_order")->queryAll();
-    }
+         $from;
+         $to;
+        return $data = Yii::$app->db->createCommand("select dat_order_date,Vchr_product_name from tbl_order JOIN tbl_product on tbl_order.Fk_int_product_id=tbl_product.Pk_int_product_id where dat_order_date between '$from' and '$to'")->queryAll();
+      }
 
 }
 
