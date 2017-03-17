43b59413ec89164518b3437be005aa63a523bf12
Athirakrishnanp <athirakrishnanp94@gmail.com>
2017-03-17 05:02:40 -0700

    Update SalesReport.php

--- models/SalesReport.php
+++ models/SalesReport.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @author athirakrishnanp
+* @author athirakrishnanp <athirakrishnanp94@gmail.com>
 * @version 1.0
 * @created date : 13/03/2017
 * @modified date : 14/03/2017
@@ -8,6 +8,7 @@
 namespace app\models;
 use Yii;
 use yii\base\Model;
+use dosamigos\highcharts\HighCharts;
 /**
  * SalesReport is the model behind the sales report.
  */
@@ -20,7 +21,7 @@ class SalesReport extends Model
     public $year;
 
     /**
-    * function rulres() for validation 
+    * @function rulres() used for form data validation
     */
     public function rules()
     {
@@ -33,13 +34,24 @@ class SalesReport extends Model
 
  /**
  * function comparedate() for comparing date from interface and  from database 
+ * @param string $from store date
+ * @param string $to store date
  */
  public function comparedate($from,$to)
     {
-         $from;
-         $to;
-        return $data = Yii::$app->db->createCommand("select dat_order_date,Vchr_product_name from tbl_order JOIN tbl_product on tbl_order.Fk_int_product_id=tbl_product.Pk_int_product_id where dat_order_date between '$from' and '$to'")->queryAll();
-      }
+        if(!(gettype($from=='string'))&&(gettype($from=='string')))
+        {
+            echo "error";
+        }
+        else
+        {
+            $from;
+            $to;
+         
+            return $data = Yii::$app->db->createCommand("select Vchr_main_Categorie,count(*) as 'num' from tbl_order JOIN tbl_product on (tbl_order.Fk_int_product_id=tbl_product.Pk_int_product_id) JOIN tbl_categories on (tbl_product.Fk_int_categories_id=tbl_categories.Pk_int_categories_id) where dat_order_date between '$from' and '$to' group by Vchr_main_Categorie")->queryAll();
+        }
+    }
 
 }
+
 
