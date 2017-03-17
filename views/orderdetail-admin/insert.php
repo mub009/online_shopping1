c4b5f1176225b8c3637cfe906851f6a83d3a7e49
fayiza <fayizatc@gmail.com>
2017-03-17 14:05:49 +0530

    Update insert.php

--- views/orderdetail-admin/insert.php
+++ views/orderdetail-admin/insert.php
@@ -3,25 +3,43 @@
 * @author fayiza 
 * @version 1.0
 * @created date 14/03/17
-* @modified date 15/03/17
+* @modified date 16/03/17
 */
 //echo "inserted";
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\widgets\ActiveField;
+use yii\helpers\ArrayHelper;
+use app\models\TblOrder;
+use app\models\TblRegistration;
 //print_r($data);
 $count=count($data);
-echo "<table>";
+?>
+<?php $form = ActiveForm::begin(); ?>
+<h2>order_details</h2>
+<table border="2" width="10px" cellpadding="5">
+<tr><td>id</td><td>first_name</td><td>last_name</td><td>mobile_number</td><td>address</td><td>product_name</td><td>price</td><td>order_id</td><td>order_date</td><td>order_status</td></tr>
+<?php 
 for($i=0;$i<$count;$i++)
 {
+	echo "<tr><td>".$data[$i]['pk_int_registration_id']."</td>";
+	echo "<td>".$data[$i]['vchr_first_name']."</td>";
+	echo "<td>".$data[$i]['vchr_last_name']."</td>";
 	
-	echo "first_name"." - ".$data[$i]['vchr_first_name']."<br>";
-	echo "last.name"." - ".$data[$i]['vchr_last_name']."<br>";
-	echo "age"." - ".$data[$i]['vchr_age']."<br>";
-	echo "mobile_number"." - ".$data[$i]['int_mobile_number']."<br>";
-	echo "address"." - ".$data[$i]['text_address']."<br>";
-	echo "order_status"." - ".$data[$i]['order_status']."<br>";
-	echo "<br>";
+	echo "<td>".$data[$i]['int_mobile_number']."</td>";
+	echo "<td>".$data[$i]['text_address']."</td>";
+	echo "<td>".$data[$i]['vchr_product_name']."</td>";
+	echo "<td>".$data[$i]['int_price']."</td>";
+	echo "<td>".$data[$i]['pk_int_order_id']."</td>";
+	echo "<td>".$data[$i]['date_order_date']."</td>";
+	//echo "</tr>";?>
 	
-}
-echo "</table>";
+	 
+              <!--echo $form->field($model, 'order_status')->dropDownList(ArrayHelper::map(TblOrder::find()->asArray()->all(),'pk_int_order_id','order_status')); -->
+	<?php echo "<td>";?><?= Html::dropDownList('listname2', $model, ['confirm'=>'confirm','processing'=>'processing', 'shipping'=>'shipping', 'delivery'=>'delivery']); ?><?php echo "</td></tr>"; ?>
+	<?php
+}?>
 
-
-?>
+	</table>
+<?php ActiveForm::end(); ?>
