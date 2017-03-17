eb8a6d37804470848e5e47ccaca51ffe657fd095
fayiza <fayizatc@gmail.com>
2017-03-17 14:03:47 +0530

    Update index.php

--- views/orderdetail-admin/index.php
+++ views/orderdetail-admin/index.php
@@ -5,18 +5,16 @@
 * @created date 14/03/17
 * @modified date 15/03/17
 */
-/* @var $this yii\web\View */
+
 use yii\helpers\Html;
 use yii\widget\ActiveForm
 ?>
 <h3><u>ORDERS</u></h3>
 
-<!--<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>-->
+
+
 <a href="index.php?r=orderdetail-admin/getuser">Order Details</a><?php echo "<br>"?>
-<a href="#">Payment Details</a><?php echo "<br>"?>
-<a href="#">Order Details</a><?php echo "<br>"?>
-<a href="#">Shipping Details</a><?php echo "<br>"?>
-<a href="#">Products</a>
+
+
+<!--<a href="#">Shipping Details</a>
+<a href="#">Products</a>-->
